88069fb852fe029eeb7980f4512c1e4c7462d8f6
Soonwook Kim <48719589+seturi@users.noreply.github.com>
2024-02-26 21:02:53 +0900

    240226

--- 02. Implementation/1.py
+++ 02. Implementation/1.py
@@ -1,14 +1,26 @@
 # 상하좌우 문제
 
-# 동, 북, 서, 남
-dx = [0, -1, 0, 1]
-dy = [1, 0, -1, 0]
+# N 입력받기
+n = int(input())
+x, y = 1, 1
+plans = input().split()
 
-# 현위치
-x, y = 2, 2
+# L, R, U, D에 따른 이동 방향
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
 
-for i in range(4):
-	# 다음 위치
-	nx = x + dx[i]
-	ny = y + dy[i]
-	print(nx, ny)
+# 이동 계획을 하나씩 확인
+for plan in plans:
+    # 이동 후 좌표 구하기
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    # 공간을 벗어나는 경우 무시
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    # 이동 수행
+    x, y = nx, ny
+
+print(x, y)
